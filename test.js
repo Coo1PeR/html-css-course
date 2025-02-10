0bcd6dc6992b0d62130b5c27dde4da101330f096
Artem Kravchenko <coo2per@gmail.com>
2025-02-10 20:53:05 +0300

    104 init main files

---
+++ test.js
@@ -0,0 +1,15 @@
+const obj1 = {
+    title: 'grip',
+    number: '+70000000001',
+    id: 75,
+    name: 'Test'
+}
+
+const obj2 = 'grip'
+
+const logObj = {...obj1};
+const logObj2 = {...obj2};
+
+
+console.log('logObj: ', logObj)
+console.log('logObj2: ', logObj2)
